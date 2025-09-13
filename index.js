8456ba83896dc890f2c49b624abe8a3c39fac226
saponeayo67-del <saponeayo67@gmail.com>
2025-09-13 21:22:32 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Service OK ✅ Ton backend marche !');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
